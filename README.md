6d843e5481440e33f5cb089f325ec1974dffdc86
abdul-korim-web <moneyschorsing@gmail.com>
2026-04-17 11:55:55 +0600

    readme file update

--- README.md
+++ README.md
@@ -1,16 +1,101 @@
-# React + Vite
+# 🤝 Friendship Management App
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+![React](https://img.shields.io/badge/React-18-blue?logo=react)
+![Tailwind](https://img.shields.io/badge/TailwindCSS-3.0-38B2AC?logo=tailwindcss)
+![Status](https://img.shields.io/badge/Project-Completed-success)
+![License](https://img.shields.io/badge/License-MIT-green)
 
-Currently, two official plugins are available:
+🔗 Live Site: https://abdul-keenkeeper.netlify.app/  
+🔗 GitHub Repo: https://github.com/abdul-korim-web/keenkeeper 
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Oxc](https://oxc.rs)
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/)
+---
 
-## React Compiler
+## 📌 Project Overview
 
-The React Compiler is not enabled on this template because of its impact on dev & build performances. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
+This is a **Friendship Management Web Application** built as part of an assignment from Programming Hero (Assignment 7).
 
-## Expanding the ESLint configuration
+Users can:
+- Manage friends
+- Search & filter data
+- Track interactions
+- View analytics dashboard
 
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+---
+
+## ✨ Features
+
+- 👥 Friend management system
+- 🔍 Live search functionality
+- 🔃 Sorting (Newest / Oldest)
+- 📊 Analytics using Recharts
+- ⏳ Timeline tracking (Call, Text, Video)
+- 📱 Fully responsive UI
+- 📭 Empty state (No data found)
+- ⚡ Fast performance
+
+---
+
+## 🛠️ Technologies Used
+
+![React](https://img.shields.io/badge/React-18-blue?logo=react)
+![Tailwind](https://img.shields.io/badge/TailwindCSS-3.0-38B2AC?logo=tailwindcss)
+![DaisyUI](https://img.shields.io/badge/DaisyUI-Component%20Library-purple)
+![React Router](https://img.shields.io/badge/React_Router-DOM-red)
+![Recharts](https://img.shields.io/badge/Recharts-Data%20Viz-orange)
+![React Icons](https://img.shields.io/badge/React_Icons-Icons-yellow)
+## 🚀 How to Run This Project
+
+```bash
+# Clone repo
+https://github.com/abdul-korim-web/keenkeeper
+
+# Open folder
+cd keenkeeper
+
+# Install dependencies
+npm install
+
+# Run project
+npm run dev
+```
+## Open in browser:
+```
+
+http://localhost:5173
+```
+## 📂 Project Structure
+```
+src/
+ ├── Components/
+ ├── Pages/
+ ├── Context/
+ ├── data/
+ ├── App.jsx
+ └── main.jsx
+ ```
+## 📦 Dependencies
+
+```bash id="dep123"
+react
+react-dom
+tailwindcss
+daisyui
+```
+## 📜 License & Copyright
+
+This project is **open-source and free to use** under the **MIT License**.  
+
+You can freely:
+- ✅ View, modify, and use the source code  
+- ✅ Distribute or include it in your own projects  
+- ✅ Use it for personal or commercial purposes  
+
+Just make sure to keep the original author credit:
+
+**© 2026 Abdul Korim**
+
+## For collaboration or inquiries, contact:  
+📧 Email: abdulkorimwebdeveloper@gmail.com <br> 
+👉 GitHub: [abdul-korim-web](https://github.com/abdul-korim-web) <br>
+👉 Facebook: [abdul-korim-web](https://www.facebook.com/abdulkorimweb) <br>
+👉 LinkedIn: [abdul-korim-web](linkedin.com/in/abdul-korim-webdeveloper) <br>
--- src/Components/Friend.jsx
+++ src/Components/Friend.jsx
@@ -4,25 +4,26 @@ import Loading from "./Loading";
 const fetchFriensdData = fetch("/data/friends.json").then((res) => res.json());
 const Friend = () => {
   const friends = use(fetchFriensdData);
+
   const [selectValue, setSelectValue] = useState("default");
-  const [SortFriendData, setsortFriendData] = useState(friends);
-  const getSortData = () => {
-    if (selectValue == "default") {
-     return setsortFriendData(friends);
+  const [searchValue, setSearchValue] = useState("");
+  const [finalData, setFinalData] = useState([]);
+
+  useEffect(() => {
+    let data = [...friends];
+    if (searchValue) {
+      data = data.filter((item) =>
+        item.name.toLowerCase().includes(searchValue.toLowerCase()),
+      );
     }
-    if (selectValue == "newest") {
-     const sortData = [...friends].sort((a,b)=>new Date(a.next_due_date) - new Date(b.next_due_date))
-     setsortFriendData(sortData)
+    if (selectValue === "newest") {
+      data.sort((a, b) => new Date(b.next_due_date) - new Date(a.next_due_date));
     }
-    if (selectValue == "oldest") {
-     const sortData = [...friends].sort((a,b)=>new Date(b.next_due_date) - new Date(a.next_due_date))
-     setsortFriendData(sortData)
+    if (selectValue === "oldest") {data.sort((a, b) => new Date(a.next_due_date) - new Date(b.next_due_date));
     }
 
-  };
-  useEffect(() => {
-    getSortData()
-  }, [selectValue]);
+    setFinalData(data);
+  }, [friends, searchValue, selectValue]);
 
   return (
     <div className="max-w-[1200px] mx-auto mt-10 px-5 py-10 md:py-20 cursor-pointer">
@@ -38,12 +39,63 @@ const Friend = () => {
           <option>oldest</option>
         </select>
       </div>
+      <div className="flex justify-center my-5">
+        <label className="input md:w-[500px] md:h-[50px] rounded-2xl">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input
+            onChange={(e) => setSearchValue(e.target.value)}
+            type="search"
+            className="grow"
+            placeholder="Search Your Friends"
+          />
+        </label>
+      </div>
       <Suspense fallback={<Loading />}>
-        <div className="mt-5 grid grid-cols-2 justify-items-center gap-3 md:grid-cols-4">
-          {SortFriendData.map((friend, index) => (
+        {finalData.length===0? (
+<div className="flex flex-col items-center justify-center py-20 text-center space-y-4">
+      
+      <div className="w-16 h-16 flex items-center justify-center rounded-full bg-[#f1f5f9] text-3xl">
+        🔍
+      </div>
+
+      <h2 className="text-xl font-semibold text-[#1F2937]">
+        No friends found
+      </h2>
+
+      <p className="text-[#64748b] max-w-[300px]">
+        No results match your search. Try a different keyword.
+      </p>
+
+      <button
+        onClick={() => setSearchValue("")}
+        className="btn px-4 py-2 bg-[#244d3f] text-white rounded-lg hover:bg-[#1b3a30] transition"
+      >
+        Clear Search
+      </button>
+
+    </div>
+        ):(
+          <div className="mt-5 grid grid-cols-2 justify-items-center gap-3 md:grid-cols-4">
+          {finalData.map((friend, index) => (
             <FriendCard key={index} friend={friend} />
           ))}
         </div>
+        )}
       </Suspense>
     </div>
   );
